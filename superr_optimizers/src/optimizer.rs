3b1dbf3809c4cc3de96aa7ba9d4a3c42e33bfd24
Alex <alex.podikoglou@gmail.com>
2023-09-08 23:54:36 +0200

    feat: work on random bruteforcing superoptimizer

--- superr_optimizers/src/optimizer.rs
+++ superr_optimizers/src/optimizer.rs
@@ -3,5 +3,5 @@ use superr_vm::program::Program;
 pub trait Optimizer {
     /// Takes a program and returns a program which is equivalent, but optimized optimally,
     /// according to the implementation.
-    fn optimize(&self, input: &Program) -> Program;
+    fn optimize(&self, input: Program) -> Program;
 }
--- superr_optimizers/src/superoptimizer.rs
+++ superr_optimizers/src/superoptimizer.rs
@@ -1,20 +1,63 @@
-use superr_vm::{instruction::Program, vm::VM};
+
+
+
+use rand::{thread_rng, Rng};
+use superr_vm::{instruction::Instruction, program::Program, vm::VM};
 
 use crate::optimizer::Optimizer;
 
-#[derive(Default)]
-pub struct Superoptimizer {}
+pub struct Superoptimizer {
+    pub max_instructions: usize,
+}
+
+impl Superoptimizer {
+    pub fn new(max_instructions: usize) -> Self {
+        Self { max_instructions }
+    }
+}
 
 impl Optimizer for Superoptimizer {
-    fn optimize(&self, input: &Program) -> Program {
-        let vm = VM::default();
+    fn optimize(&self, input: Program) -> Program {
+        let mut vm = VM::default();
 
-        vec![]
+        vm.execute_program(input);
+        let target_state = vm.state.clone();
+
+        vm.reset();
+
+        loop {
+            let mut instructions = vec![];
+
+            for _ in 0..8 {
+                let instruction_index = thread_rng().gen_range(0..=3);
+                let arg01: usize = thread_rng().gen_range(0..4);
+                let arg02: usize = thread_rng().gen_range(0..4);
+
+                match instruction_index {
+                    0 => instructions.push(Instruction::Load(arg01 as u32)),
+                    1 => instructions.push(Instruction::Swap(arg01, arg02)),
+                    2 => instructions.push(Instruction::XOR(arg01, arg02)),
+                    3 => instructions.push(Instruction::Inc(arg01)),
+
+                    _ => {}
+                }
+            }
+
+            let program = Program { instructions };
+
+            vm.execute_program(program.clone());
+
+            if vm.state == target_state {
+                return program;
+            }
+
+            vm.reset();
+        }
     }
 }
 
 mod test {
-    use superr_vm::{instruction::Instruction, vm::VM};
+    use superr_vm::{instruction::Instruction, program::Program, vm::VM};
 
     use crate::optimizer::Optimizer;
 
@@ -22,22 +65,24 @@ mod test {
 
     #[test]
     fn optimize_3() {
-        let original = vec![
-            Instruction::Load(3),
-            Instruction::Swap(0x00, 0x01),
-            Instruction::Load(3),
-            Instruction::Swap(0x00, 0x02),
-            Instruction::Load(3),
-            Instruction::Swap(0x00, 0x03),
-            Instruction::Load(3),
-        ];
+        let original = Program {
+            instructions: vec![
+                Instruction::Load(3),
+                Instruction::Swap(0x00, 0x01),
+                Instruction::Load(3),
+                Instruction::Swap(0x00, 0x02),
+                Instruction::Load(3),
+                Instruction::Swap(0x00, 0x03),
+                Instruction::Load(3),
+            ],
+        };
 
         let mut vm_original = VM::default();
         vm_original.execute_program(original.clone());
 
         // optimize
-        let optimizer = Superoptimizer::default();
-        let optimized = optimizer.optimize(&original);
+        let optimizer = Superoptimizer::new(4);
+        let optimized = optimizer.optimize(original);
 
         let mut vm_optimized = VM::default();
         vm_optimized.execute_program(optimized);
