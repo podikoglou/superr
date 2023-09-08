4090a74ffa280acf647c1ed06658bc0d541b59b3
Alex <alex.podikoglou@gmail.com>
2023-09-08 13:46:11 +0200

    refactor: represent addresses in hex

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -7,11 +7,11 @@ fn main() {
 
     let program = vec![
         Instruction::Load(3),
-        Instruction::Swap(0, 1),
+        Instruction::Swap(0x00, 0x01),
         Instruction::Load(3),
-        Instruction::Swap(0, 2),
+        Instruction::Swap(0x00, 0x02),
         Instruction::Load(3),
-        Instruction::Swap(0, 3),
+        Instruction::Swap(0x00, 0x03),
         Instruction::Load(3),
     ];
 
