2ce7200914decec9ab6ec4b260dcbb3764c153fa
Alex <alex.podikoglou@gmail.com>
2023-09-09 18:41:15 +0200

    doc: document superoptimizer algorithm

--- superr_optimizers/src/superoptimizer.rs
+++ superr_optimizers/src/superoptimizer.rs
@@ -25,37 +25,44 @@ impl Superoptimizer {
 
 impl Optimizer for Superoptimizer {
     fn optimize(&self, input: Program) -> Program {
-        // create vm
+        let started = Instant::now();
+
+        // create VM for testing
         let mut vm = VM::default();
 
+        // execute original program to determine target state
         vm.execute_program(input.clone());
 
         let target_state = vm.state.clone();
 
-        dbg!(target_state);
+        // move the original program to the `shortest` variable, as it is the shortest version of
+        // the program we have.
+        let mut shortest = input;
 
-        // state
-        let started = Instant::now();
-
-        let mut shortest: Program = Program {
-            instructions: vec![],
-        };
-
-        // start bruteforcing optimal programs
+        // start generating absolutely random program in hopes that one of them is equivalent to
+        // the original one.
         loop {
-            // generate program
             let mut instructions = vec![];
 
-            let instructions_amount = rand::thread_rng().gen_range(1..=self.max_instructions);
+            // generate a random amount of instructions for the program to have. this amount is
+            // within 0 and the given max_instructions.
+            let instructions_amount = rand::thread_rng().gen_range(0..=self.max_instructions);
 
+            // generate the program instructions
             for _ in 0..instructions_amount {
+                // generate a random instruction by its index (0-3)
                 let instruction_index = thread_rng().gen_range(0..=3);
 
+                // generate two random addresses. the second one is redundant for instructions like
+                // Inc and Load.
                 let addr01: usize = thread_rng().gen_range(0..vm::MEM_SIZE);
                 let addr02: usize = thread_rng().gen_range(0..vm::MEM_SIZE);
 
+                // generates a random value. this is reundant for instructions other than load.
                 let val: usize = thread_rng().gen_range(0..32);
 
+                // convert the instruction_index we generater earlier and the arguments we just
+                // generated to an actual instruction and add it to the instructions list.
                 match instruction_index {
                     0 => instructions.push(Instruction::Load(val as u32)),
                     1 => instructions.push(Instruction::Swap(addr01, addr02)),
@@ -66,18 +73,19 @@ impl Optimizer for Superoptimizer {
                 }
             }
 
-            // execute program
+            // actually create the program
             let program = Program { instructions };
 
+            // reset the memory from the previous iteration and execute the program to see if the
+            // state matches.
             vm.reset();
             vm.execute_program(program.clone());
 
-            dbg!(vm.state);
-
-            // is it equivalent to the input program?
-            // if so, check if it's also the shortest equivalent
+            // check if this program is equivalent to the given one by checking if the states they
+            // produce are equal.
             if vm.state == target_state {
-                // if it's the shortest one encountered (or te first equivalent), set it as the shortest
+                // if the state is equiavlent to the target state, check if this is the shortest
+                // equivalent program we've encountered. if so, set it to the shortest variable.
                 if shortest.instructions.len() == 0
                     || shortest.instructions.len() > program.instructions.len()
                 {
@@ -85,50 +93,10 @@ impl Optimizer for Superoptimizer {
                 }
             }
 
-            // if we're out of time return, return the shortest one we found
+            // if we're out of time, return the shortest program we've found
             if started.elapsed() >= self.max_time {
-                // if we haven't found any optimized version of the given program, just return the
-                // original.
-                if shortest.instructions.len() == 0 {
-                    return input;
-                }
                 return shortest;
             }
         }
-    }
-}
-
-mod test {
-    use superr_vm::{instruction::Instruction, program::Program, vm::VM};
-
-    use crate::optimizer::Optimizer;
-
-    use super::Superoptimizer;
-
-    #[test]
-    fn optimize_3() {
-        let original = Program {
-            instructions: vec![
-                Instruction::Load(3),
-                Instruction::Swap(0x00, 0x01),
-                Instruction::Load(3),
-                Instruction::Swap(0x00, 0x02),
-                Instruction::Load(3),
-                Instruction::Swap(0x00, 0x03),
-                Instruction::Load(3),
-            ],
-        };
-
-        let mut vm_original = VM::default();
-        vm_original.execute_program(original.clone());
-
-        // optimize
-        let optimizer = Superoptimizer::new(4);
-        let optimized = optimizer.optimize(original);
-
-        let mut vm_optimized = VM::default();
-        vm_optimized.execute_program(optimized);
-
-        assert_eq!(vm_original.state, vm_optimized.state);
     }
 }
