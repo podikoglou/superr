b3db6dcefb313dbfa1468a0242baae5d4eefb4ee
Alex <alex.podikoglou@gmail.com>
2024-07-13 00:53:05 +0300

    feat: finalize the superoptimization algorithm

--- superr_optimizers/src/superoptimizer.rs
+++ superr_optimizers/src/superoptimizer.rs
@@ -11,14 +11,14 @@ use crate::optimizer::Optimizer;
 
 pub struct Superoptimizer {
     pub max_instructions: usize,
-    pub max_time: Duration,
+    pub timeout: Duration,
 }
 
 impl Superoptimizer {
-    pub fn new(max_instructions: usize, max_time: Duration) -> Self {
+    pub fn new(max_instructions: usize, timeout: Duration) -> Self {
         Self {
             max_instructions,
-            max_time,
+            timeout,
         }
     }
 }
@@ -31,7 +31,7 @@ impl Optimizer for Superoptimizer {
         let mut vm = VM::default();
 
         // execute original program to determine target state
-        vm.execute_program(input.clone());
+        vm.execute_program(&input);
 
         let target_state = vm.state.clone();
 
@@ -39,62 +39,53 @@ impl Optimizer for Superoptimizer {
         // the program we have.
         let mut shortest = input;
 
-        // start generating absolutely random program in hopes that one of them is equivalent to
+        // start generating absolutely random programs in hopes that one of them is equivalent to
         // the original one.
         loop {
-            let mut instructions = vec![];
+            let mut program = Program::new();
+
+            let mut rng = rand::thread_rng();
 
             // generate a random amount of instructions for the program to have. this amount is
             // within 0 and the given max_instructions.
-            let instructions_amount = rand::thread_rng().gen_range(0..=self.max_instructions);
+            let instructions_amount = rng.gen_range(0..=self.max_instructions);
 
-            // generate the program instructions
+            // generate the instructions of the program
             for _ in 0..instructions_amount {
-                // generate a random instruction by its index (0-3)
-                let instruction_index = thread_rng().gen_range(0..=3);
+                let reg1 = rng.gen_range(0..vm::MEM_SIZE);
+                let reg2 = rng.gen_range(0..vm::MEM_SIZE);
 
-                // generate two random addresses. the second one is redundant for instructions like
-                // Inc and Load.
-                let addr01: usize = thread_rng().gen_range(0..vm::MEM_SIZE);
-                let addr02: usize = thread_rng().gen_range(0..vm::MEM_SIZE);
+                let val = rng.gen_range(0..32);
 
-                // generates a random value. this is reundant for instructions other than load.
-                let val: usize = thread_rng().gen_range(0..32);
+                let instruction = rng.gen_range(0..=3);
 
-                // convert the instruction_index we generater earlier and the arguments we just
-                // generated to an actual instruction and add it to the instructions list.
-                match instruction_index {
-                    0 => instructions.push(Instruction::Load(val as u32)),
-                    1 => instructions.push(Instruction::Swap(addr01, addr02)),
-                    2 => instructions.push(Instruction::XOR(addr01, addr02)),
-                    3 => instructions.push(Instruction::Inc(addr01)),
-
-                    _ => {}
-                }
+                let instruction = match instruction {
+                    0 => Instruction::Load(val),
+                    1 => Instruction::Swap(reg1, reg2),
+                    2 => Instruction::XOR(reg1, reg2),
+                    3 => Instruction::Inc(reg1),
+                    _ => panic!("SUPER unexpected error occurred"),
+                };
+                program.instructions.push(instruction);
             }
-
-            // actually create the program
-            let program = Program { instructions };
 
             // reset the memory from the previous iteration and execute the program to see if the
             // state matches.
             vm.reset();
-            vm.execute_program(program.clone());
+            vm.execute_program(&program);
 
             // check if this program is equivalent to the given one by checking if the states they
             // produce are equal.
             if vm.state == target_state {
-                // if the state is equiavlent to the target state, check if this is the shortest
+                // if the state is equivalent to the target state, check if this is the shortest
                 // equivalent program we've encountered. if so, set it to the shortest variable.
-                if shortest.instructions.len() == 0
-                    || shortest.instructions.len() > program.instructions.len()
-                {
+                if shortest.instructions.len() > program.instructions.len() {
                     shortest = program.clone();
                 }
             }
 
             // if we're out of time, return the shortest program we've found
-            if started.elapsed() >= self.max_time {
+            if started.elapsed() >= self.timeout {
                 return shortest;
             }
         }
