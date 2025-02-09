33853952c0214a170b135b240fd61fdce4dbb7a6
Alex <alex.podikoglou@gmail.com>
2025-02-09 20:54:15 +0200

    test(qua_lexer): basic tests

--- qua_lexer/src/lib.rs
+++ qua_lexer/src/lib.rs
@@ -50,3 +50,137 @@ pub fn lex(source: String) -> Vec<Token> {
 
     tokens
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_tokens_eq(input: &str, expected: Vec<Token>) {
+        let result = lex(input.to_string());
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_int_literal() {
+        assert_tokens_eq("0", vec![Token::IntLiteral(0), Token::EOF]);
+        assert_tokens_eq("1", vec![Token::IntLiteral(1), Token::EOF]);
+        assert_tokens_eq(
+            "4294967294",
+            vec![Token::IntLiteral(4294967294), Token::EOF],
+        );
+        assert_tokens_eq(
+            "4294967295",
+            vec![Token::IntLiteral(4294967295), Token::EOF],
+        );
+    }
+
+    #[test]
+    fn test_float_literal() {
+        assert_tokens_eq("0.0", vec![Token::FloatLiteral(0.0), Token::EOF]);
+        assert_tokens_eq("0.1", vec![Token::FloatLiteral(0.1), Token::EOF]);
+        assert_tokens_eq("1.0", vec![Token::FloatLiteral(1.0), Token::EOF]);
+        assert_tokens_eq("1.1", vec![Token::FloatLiteral(1.1), Token::EOF]);
+        assert_tokens_eq(
+            "3.141592653589793238462643383279502884197",
+            vec![
+                Token::FloatLiteral(3.141592653589793238462643383279502884197),
+                Token::EOF,
+            ],
+        );
+    }
+
+    #[test]
+    fn test_string_literal() {
+        assert_tokens_eq(
+            "\"Qua!\"",
+            vec![Token::StringLiteral("Qua!".to_string()), Token::EOF],
+        );
+
+        assert_tokens_eq(
+            "\"\"",
+            vec![Token::StringLiteral("".to_string()), Token::EOF],
+        );
+    }
+
+    #[test]
+    fn test_delimiters() {
+        // parentheses
+        assert_tokens_eq("(", vec![Token::OpenParen, Token::EOF]);
+        assert_tokens_eq(")", vec![Token::CloseParen, Token::EOF]);
+
+        assert_tokens_eq("()", vec![Token::OpenParen, Token::CloseParen, Token::EOF]);
+        assert_tokens_eq(
+            "(())",
+            vec![
+                Token::OpenParen,
+                Token::OpenParen,
+                Token::CloseParen,
+                Token::CloseParen,
+                Token::EOF,
+            ],
+        );
+        assert_tokens_eq(
+            "(()",
+            vec![
+                Token::OpenParen,
+                Token::OpenParen,
+                Token::CloseParen,
+                Token::EOF,
+            ],
+        );
+
+        // braces
+        assert_tokens_eq("{", vec![Token::OpenBrace, Token::EOF]);
+        assert_tokens_eq("}", vec![Token::CloseBrace, Token::EOF]);
+
+        assert_tokens_eq("{}", vec![Token::OpenBrace, Token::CloseBrace, Token::EOF]);
+        assert_tokens_eq(
+            "{{}}",
+            vec![
+                Token::OpenBrace,
+                Token::OpenBrace,
+                Token::CloseBrace,
+                Token::CloseBrace,
+                Token::EOF,
+            ],
+        );
+        assert_tokens_eq(
+            "{{}",
+            vec![
+                Token::OpenBrace,
+                Token::OpenBrace,
+                Token::CloseBrace,
+                Token::EOF,
+            ],
+        );
+
+        // brackets
+        assert_tokens_eq("[", vec![Token::OpenBracket, Token::EOF]);
+        assert_tokens_eq("]", vec![Token::CloseBracket, Token::EOF]);
+
+        assert_tokens_eq(
+            "[]",
+            vec![Token::OpenBracket, Token::CloseBracket, Token::EOF],
+        );
+        assert_tokens_eq(
+            "[[]]",
+            vec![
+                Token::OpenBracket,
+                Token::OpenBracket,
+                Token::CloseBracket,
+                Token::CloseBracket,
+                Token::EOF,
+            ],
+        );
+        assert_tokens_eq(
+            "[[]",
+            vec![
+                Token::OpenBracket,
+                Token::OpenBracket,
+                Token::CloseBracket,
+                Token::EOF,
+            ],
+        );
+    }
+}
