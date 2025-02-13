8880deed34831bae5fde995781bdbd6bd5cf4772
Alex <alex.podikoglou@gmail.com>
2025-02-13 17:32:31 +0200

    feat(cli): exclude logic

--- superr/src/cli/gen.rs
+++ superr/src/cli/gen.rs
@@ -11,15 +11,26 @@ pub fn execute(matches: &ArgMatches) -> anyhow::Result<()> {
     let min_imm = matches.get_one::<u8>("min-imm").unwrap();
     let max_imm = matches.get_one::<u8>("max-imm").unwrap();
 
+    let excluded: Vec<&String> = match matches.get_many("exclude") {
+        Some(v) => v.collect(),
+        None => vec![],
+    };
+
+    // TODO: clean this up
+    let ins_space: Vec<&str> = INSTRUCTIONS
+        .into_iter()
+        .filter(|ins| !excluded.contains(&&(*ins.to_string()).to_string()))
+        .collect();
+
     for _ in 0..fastrand::usize(*min_instructions..=*max_instructions) {
         let addr1 = fastrand::usize(0..vm::MEM_SIZE);
         let addr2 = fastrand::usize(0..vm::MEM_SIZE);
 
         let imm = fastrand::u8(min_imm..=max_imm);
 
-        let choice = fastrand::choice(INSTRUCTIONS).context("invalid iterator length")?;
+        let choice = fastrand::choice(&ins_space).context("invalid iterator length")?;
 
-        let instruction = match choice {
+        let instruction = match *choice {
             "load" => Instruction::Load(imm),
             "swap" => Instruction::Swap(addr1, addr2),
             "xor" => Instruction::XOR(addr1, addr2),
